9c0695dcb33557e1e14fcc484b88c4f6b31fde4a
Rogopl <79219873+Rogopl@users.noreply.github.com>
2021-03-13 23:34:04 +0300

    Changed output

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,9 @@
 #include <string>
 //commentari 1
 int main(int argc, char** argv)
-{ // commentariii2
+{ // commentari2
 std::string a;
-std::cout << "Please insert your name: "
+std::cout << "Please, insert your username: "
 std::cin >> a;
 std::cout << std::endl << "Hello world from: " << a;
 }
