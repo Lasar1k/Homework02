509463151bc4239db469945d51247cb33a488ba4
Rogopl <alf.ivan2002@gmail.com>
2021-03-13 23:16:04 +0300

    Changed code style to Mozilla

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
-//commentari 1
-int main(int argc, char** argv)
+// commentari 1
+int
+main(int argc, char** argv)
 { // commentariii2
-std::string a;
-std::cout << "Please insert your name: "
-std::cin >> a;
-std::cout << std::endl << "Hello world from: " << a;
+  std::string a;
+  std::cout << "Please insert your name: " std::cin >> a;
+  std::cout << std::endl << "Hello world from: " << a;
 }
